79b7e9f8b35c7fc238f9e985f65589cac1872a12
Viktor Gubarevich <gubarew.v@gmail.com>
2021-07-13 08:32:42 +0300

    updated sagas

--- src/_actions/sagas.js
+++ src/_actions/sagas.js
@@ -1,38 +1,33 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import { logIn, signUp } from "./storageApi";
 
-function* logInSaga({ payload }) {
+function* saga(callback, { payload }, { success, fail }) {
   try {
     const { username, password } = payload;
-    yield call(logIn, username, password);
-    yield put({ type: "USERS_LOGIN_SUCCESS", payload: username });
+    yield call(callback, username, password);
+    yield put({ type: success, payload: username });
   } catch (error) {
     yield put({
-      type: "USERS_LOGIN_FAILURE",
+      type: fail,
       payload: error.message,
       error: true,
     });
   }
 }
 
-function* signUpSaga({ payload }) {
-  try {
-    const { username, password } = payload;
-    yield call(signUp, username, password);
-    yield put({ type: "USERS_REGISTER_SUCCESS", payload: username });
-  } catch (error) {
-    yield put({
-      type: "USERS_REGISTER_FAILURE",
-      payload: error.message,
-      error: true,
-    });
-  }
-}
+const logInSaga = (attr) =>
+  saga(logIn, attr, {
+    success: "USERS_LOGIN_SUCCESS",
+    fail: "USERS_LOGIN_FAILURE",
+  });
 
+const signUpSaga = (attr) =>
+  saga(signUp, attr, {
+    success: "USERS_REGISTER_SUCCESS",
+    fail: "USERS_REGISTER_FAILURE",
+  });
 
 export default function* rootSaga() {
   yield takeLatest("LOG_IN", logInSaga);
   yield takeLatest("SIGN_UP", signUpSaga);
 }
-
-
--- src/_actions/storageApi.js
+++ src/_actions/storageApi.js
@@ -1,31 +1,27 @@
 import { loadState } from "../store/localStorage";
 
 export const logIn = (username, password) =>
-
   new Promise((resolve, reject) => {
-
     const store = loadState().users;
 
-    if (typeof store[username] === 'undefined') {
+    if (typeof store[username] === "undefined") {
       reject(new Error("Incorrect username."));
     } else if (store[username].password !== password) {
       reject(new Error("Incorrect password."));
     } else {
-      resolve()
+      resolve();
     }
   });
 
 export const signUp = (username, password) =>
-
   new Promise((resolve, reject) => {
-
     const store = loadState().users;
 
-    if (typeof store[username] === 'undefined') {
+    if (typeof store[username] === "undefined") {
       reject(new Error("Incorrect username."));
     } else if (store[username].password !== password) {
       reject(new Error("Incorrect password."));
     } else {
-      resolve()
+      resolve();
     }
   });
